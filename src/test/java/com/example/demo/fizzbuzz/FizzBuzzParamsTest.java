aae00bf471cff91a7453660bb672c4cf5ae649f6
supaket <supaket@enersys.co.th>
2023-09-29 11:57:37 +0700

    Add input.csv test

--- src/test/java/com/example/demo/fizzbuzz/FizzBuzzParamsTest.java
+++ src/test/java/com/example/demo/fizzbuzz/FizzBuzzParamsTest.java
@@ -2,6 +2,7 @@ package com.example.demo.fizzbuzz;
 
 import com.example.demo.FizzBuzz.FizzBuzz;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzParamsTest {
 
     @ParameterizedTest
-    @ValueSource (ints = {3, 6, 9})
+    @ValueSource(ints = {3, 6, 9})
     public void shouldSayFizzWhenDivideByThree(int input) {
         // AAA
 
@@ -27,7 +28,7 @@ public class FizzBuzzParamsTest {
 
     @ParameterizedTest
     @ValueSource(ints = {5, 10})
-    public void shouldSayBuzzWhenDivideByFive(int input){
+    public void shouldSayBuzzWhenDivideByFive(int input) {
 
         // Arrange
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -42,7 +43,7 @@ public class FizzBuzzParamsTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,4,7,8})
+    @ValueSource(ints = {1, 2, 4, 7, 8})
     public void shouldSayNumberWhenNotDivideByThreeOrFive(int input) {
         //arrange
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -70,28 +71,41 @@ public class FizzBuzzParamsTest {
 
     @ParameterizedTest
     @CsvSource({
-        "1, 1",
-        "2, 2",
-        "3, Fizz",
-        "4, 4",
-        "5, Buzz",
-        "6, Fizz",
-        "7, 7",
-        "8, 8",
-        "9, Fizz",
-        "10, Buzz",
-        "15, FizzBuzz"
+            "1, 1",
+            "2, 2",
+            "3, Fizz",
+            "4, 4",
+            "5, Buzz",
+            "6, Fizz",
+            "7, 7",
+            "8, 8",
+            "9, Fizz",
+            "10, Buzz",
+            "15, FizzBuzz"
     })
-   public void shouldSayFizzBuzzOrNumberAsInputProvide(int input, String expected) {
-       //Arrange
-       FizzBuzz fizzBuzz = new FizzBuzz();
+    public void shouldSayFizzBuzzOrNumberAsInputProvide(int input, String expected) {
+        //Arrange
+        FizzBuzz fizzBuzz = new FizzBuzz();
 
-       //ACT
-       String result = fizzBuzz.say(input);
+        //ACT
+        String result = fizzBuzz.say(input);
 
-       //Assert
+        //Assert
         assertEquals(expected, result);
 
-   }
+    }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/input.csv")
+    public void shouldSayFizzBuzzOrNumberAsFileProvide(int input, String expected) {
+        //Arrange
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //ACT
+        String result = fizzBuzz.say(input);
+
+        //Assert
+        assertEquals(expected, result);
+
+    }
 }
